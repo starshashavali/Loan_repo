b8320ff58449780fae727449f10c6e7e225111ed
shasha <javashashavali@gmail.com>
2023-07-08 11:22:17 +0530

    Number reverse programming

---
+++ Test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Test{
+public static void printNumRevOrder(int num){
+while(num!=0){
+System.out.print(num%10);
+num=num/10;
+}
+}
+
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+printNumRevOrder(num);
+}}
+
